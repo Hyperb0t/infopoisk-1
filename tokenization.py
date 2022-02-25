ed723b3e3c5b40fdbf1af7a82c381c8a4a0f2784
hyperbot <vitalij-groshev@mail.ru>
2022-02-25 08:50:12 +0300

    minor fixes

--- tokenization.py
+++ tokenization.py
@@ -6,6 +6,7 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 
 if __name__ == '__main__':
+    nltk.download('punkt')
     nltk.download('stopwords')
     nltk.download('wordnet')
     nltk.download('omw-1.4')
